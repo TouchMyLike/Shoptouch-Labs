21bd359ad52cb81855dc26a24de059996a2f2f7c
KhunNooDev <noo_okok@hotmail.com>
2023-09-01 23:10:26 +0700

    login with github

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,17 +2,30 @@
 import React from 'react'
 import { useSession, signIn } from 'next-auth/react'
 import { redirect } from 'next/navigation'
+import { FaGoogle, FaGithub } from 'react-icons/fa'
 
-export default function page() {
+export default function Page() {
   const { data: session } = useSession()
   if (session) {
     redirect('/')
   }
   return (
-    <div>
-      <button onClick={() => signIn('google')} className='rounded-lg bg-black p-2 text-white'>
-        login google
-      </button>
+    <div className='flex min-h-screen items-center justify-center'>
+      <div className='rounded bg-white p-8 shadow-md'>
+        <h1 className='mb-6 text-3xl font-semibold'>Login</h1>
+        <button
+          className='mb-2 flex items-center rounded bg-red-500 px-4 py-2 font-semibold text-white hover:bg-red-600'
+          onClick={() => signIn('google')}
+        >
+          <FaGoogle className='mr-2' /> Login with Google
+        </button>
+        <button
+          className='flex items-center rounded bg-gray-900 px-4 py-2 font-semibold text-white hover:bg-gray-800'
+          onClick={() => signIn('github')}
+        >
+          <FaGithub className='mr-2' /> Login with GitHub
+        </button>
+      </div>
     </div>
   )
 }
